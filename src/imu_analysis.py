4f023cddaed2844ee208b9310e122840cc0bc84f
Manjunath, Shreyas <shreyas.manjunath@iosb.fraunhofer.de>
2020-10-29 10:54:29 +0100

    First commit

---
+++ src/imu_analysis.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+
+import rospy
+import rosbag
+import yaml, csv
+from time import sleep
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+bag_name = "/home/manshr/Test_Flight_2_2020-10-22-10-32-13.bag"
+rostopic_name = "/dji_osdk_ros/imu"
+	
+
+def analyse_imu():
+	bag = rosbag.Bag(bag_name)
+	bag_info = yaml.load(bag._get_yaml_info())
+	file_to_be_opened = "/home/manshr/Imu_data.csv"
+	csvfile = open(file_to_be_opened, 'wb')
+	writer = csv.writer(csvfile, delimiter=',')
+
+	for msg in bag.read_messages(rostopic_name):
+		current_time_stamp = rospy.Time.now()
+		print(msg.message)
+		gyroscope_data = msg.message.angular_velocity
+		accelerometer_data = msg.message.linear_acceleration
+		#orientation = msg.message.orientation		
+		row = [current_time_stamp, gyroscope_data.x,gyroscope_data.y,gyroscope_data.z,accelerometer_data.x,accelerometer_data.y,accelerometer_data.z]
+		
+			
+		writer.writerow(row)
+
+	print("Done writing into csv")
+
+
+
+def plot_data(file):
+	t = []
+	gyro_x = []
+	gyro_y = []
+	gyro_z = []
+
+	with open(file,'r') as csvfile:
+	    plots = csv.reader(csvfile, delimiter=',')
+	    for row in plots:
+		t.append(float(row[0]))
+		gyro_x.append(float(row[1]))
+		gyro_y.append(float(row[2]))
+		gyro_z.append(float(row[3]))
+	
+	plt.figure(1)
+	plt.plot(t,gyro_x, label='gyro_x', color='b',marker="x")
+	plt.xlabel('t')
+	plt.ylabel('gyro_x')
+	plt.title('IMU_Gyro_x')
+	plt.legend()
+
+	plt.figure(2)
+	plt.plot(t,gyro_y, label='gyro_y', color='g',marker='o')
+	plt.xlabel('t')
+	plt.ylabel('gyro_y')
+	plt.title('IMU_Gyro_y')
+	plt.legend()
+
+	plt.figure(3)
+	plt.plot(t,gyro_z, label='gyro_z', color='r',marker='v')
+	plt.xlabel('t')
+	plt.ylabel('gyro_z')
+	plt.title('IMU_Gyro_z')
+	plt.legend()
+	plt.show()
+
+
+	
+	
+
+if __name__=='__main__':
+	rospy.init_node("imu_analysis_node")
+	#analyse_imu()
+	plot_data("/home/manshr/Imu_data.csv")
+	rospy.spin()
+	
